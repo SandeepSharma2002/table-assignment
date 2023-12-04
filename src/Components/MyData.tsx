dca7ad350cdff6e38f512b5d167585d6e087fa6b
sandeepsharma <sandeepsharmajec24@gmail.com>
2023-12-05 01:42:03 +0530

    changes

--- src/Components/MyData.tsx
+++ src/Components/MyData.tsx
@@ -277,7 +277,7 @@ export const MyData = () => {
                   className="bg-blue-700 text-white active:bg-emerald-600 font-medium uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ml-auto"
               
                 >
-                  Add Staff
+                  Add New User
                 </button>
             </div>
                 
